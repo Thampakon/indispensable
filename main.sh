89483d9931b354895d90dba0b34b05e7fbf160d9
Thampakon <119696243+Thampakon@users.noreply.github.com>
2023-10-15 03:18:03 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -7,3 +7,4 @@ curl -O https://raw.githubusercontent.com/Thampakon/indispensable/main/update.sh
 bash update.sh
 curl -O https://raw.githubusercontent.com/Thampakon/indispensable/main/setup.sh
 bash setup.sh
+curl -O https://raw.githubusercontent.com/Thampakon/indispensable/main/setup.py
