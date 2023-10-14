a0b20639ba7dcd724da96a243964b5d1c914ec27
Thampakon <119696243+Thampakon@users.noreply.github.com>
2023-10-15 02:25:57 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 su
-apt install sqlmap
-apt install burpsuite
-apt install beef-xss
-apt install netcat
-apt install subfinder
-apt install nmap
+
+# install Tool
+apt install -y tor
+apt install -y python
+apt install -y sqlmap
+apt install -y burpsuite
+apt install -y beef-xss
+apt install -y netcat
+apt install -y subfinder
+apt install -y nmap
+apt install -y docker.io
 cd $HOME/Desktop
 curl https://raw.githubusercontent.com/21y4d/nmapAutomator/master/nmapAutomator.sh > $HOME/Desktop
 git clone https://github.com/stormshadow07/BeeF-Over-Wan.git
+
+sqlmap --update
 
