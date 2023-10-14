0138c4fbe4b4bbf5fa5282b44e915564d091a319
Thampakon <119696243+Thampakon@users.noreply.github.com>
2023-10-15 03:24:15 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,10 @@
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 su
-
+curl -O https://raw.githubusercontent.com/Thampakon/indispensable/main/setup.py
 curl -O https://raw.githubusercontent.com/Thampakon/indispensable/main/install.sh
 bash install.sh
 curl -O https://raw.githubusercontent.com/Thampakon/indispensable/main/update.sh
 bash update.sh
 curl -O https://raw.githubusercontent.com/Thampakon/indispensable/main/setup.sh
 bash setup.sh
-curl -O https://raw.githubusercontent.com/Thampakon/indispensable/main/setup.py
+
