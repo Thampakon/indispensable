40b74b9e0eae44085d0c40affc0a12d98940f29d
Thampakon <119696243+Thampakon@users.noreply.github.com>
2023-10-15 02:41:14 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,3 +14,4 @@ apt install -y docker.io
 cd $HOME/Desktop
 curl -O https://raw.githubusercontent.com/21y4d/nmapAutomator/master/nmapAutomator.sh > $HOME/Desktop
 git clone https://github.com/stormshadow07/BeeF-Over-Wan.git
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
