323cca9b33566f862ca9904f45bc0f6278cb19f3
Thampakon <119696243+Thampakon@users.noreply.github.com>
2023-10-15 02:38:44 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 su
 
-curl https://raw.githubusercontent.com/Thampakon/indispensable/main/install.sh
+curl -O https://raw.githubusercontent.com/Thampakon/indispensable/main/install.sh
 bash install.sh
-curl https://raw.githubusercontent.com/Thampakon/indispensable/main/update.sh
+curl -O https://raw.githubusercontent.com/Thampakon/indispensable/main/update.sh
 bash update.sh
