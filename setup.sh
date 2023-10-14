4c3b82ce1696ca08a394d900f0960b5d08397b75
Thampakon <119696243+Thampakon@users.noreply.github.com>
2023-10-15 03:40:19 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
-su
 
 read -p "token ngrok:" ngrok
-auth_token=$(ngrok config add-authtoken $ngrok | sed -n 's/.*Authtoken saved to configuration file: //p')
+auth_token=$(sudo ngrok config add-authtoken $ngrok | sed -n 's/.*Authtoken saved to configuration file: //p')
 
 export AUTH_TOKEN=$auth_token
 
-python setup.py --fix $AUTH_TOKEN --authtoken $ngrok
+sudo python setup.py --fix $AUTH_TOKEN --authtoken $ngrok
 
