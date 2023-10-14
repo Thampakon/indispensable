a4af6d931221eaf57f3eadc2e57fd6cd32c25e23
Thampakon <119696243+Thampakon@users.noreply.github.com>
2023-10-15 02:20:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ apt install beef-xss
 apt install netcat
 apt install subfinder
 apt install nmap
+cd $HOME/Desktop
 curl https://raw.githubusercontent.com/21y4d/nmapAutomator/master/nmapAutomator.sh > $HOME/Desktop
+git clone https://github.com/stormshadow07/BeeF-Over-Wan.git
 
