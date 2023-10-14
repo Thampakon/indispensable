45a27ed621c333550f23382d5fd88ef7917c4f1e
Thampakon <119696243+Thampakon@users.noreply.github.com>
2023-10-15 02:30:37 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,3 +15,4 @@ cd $HOME/Desktop
 curl https://raw.githubusercontent.com/21y4d/nmapAutomator/master/nmapAutomator.sh > $HOME/Desktop
 git clone https://github.com/stormshadow07/BeeF-Over-Wan.git
 
+rm install.sh
